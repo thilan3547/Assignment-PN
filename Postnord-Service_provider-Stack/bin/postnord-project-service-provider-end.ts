92aae18f8f6735ae70b5d676822dd293a093cd0b
thilan3547 <86346497+thilan3547@users.noreply.github.com>
2023-08-22 12:17:00 +0200

    Delete Postnord-Service_provider-Stack directory

--- Postnord-Service_provider-Stack/bin/postnord-project-service-provider-end.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { PNProjectServiceProviderEndStack } from '../lib/postnord-project-service-provider-end-stack';
-
-const app = new cdk.App();
-new PNProjectServiceProviderEndStack(app, 'PostnordProjectServiceProviderEndStack', {});
