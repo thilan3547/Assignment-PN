261ea540bc3f38172958bc9f2206a3ddce2ae51a
thilan3547 <thilan3547@gmail.com>
2023-08-22 22:23:24 +0200

    git action workflow

--- Postnord-Service-Consumer-Stack/test/postnord-service-consumer-stack.test.ts
+++ Postnord-Service-Consumer-Stack/test/postnord-service-consumer-stack.test.ts
@@ -1,17 +1,28 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as PostnordServiceConsumerStack from '../lib/postnord-service-consumer-stack-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as PostnordServiceConsumerStack from '../lib/postnord-service-consumer-stack-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/postnord-service-consumer-stack-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new PostnordServiceConsumerStack.PostnordServiceConsumerStackStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe('Stack Testing', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new PostnordServiceConsumerStack.PostnordServiceConsumerStackStack(app, 'MyTestStack');
+    // THEN
+    const template = Template.fromStack(stack);
+
+    test('Lambda creation', () => {
+        template.hasResourceProperties('AWS::Lambda::Function', {
+            MemorySize: 128,
+            Timeout: 60,
+            Handler: 'lambda_function.lambda_handler',
+            Runtime: 'python3.10'
+        })
+    });
+    test('Vpc creation', () => {
+        template.hasResourceProperties('AWS::EC2::VPC', {
+            EnableDnsHostnames: true,
+            EnableDnsSupport: true
+        })
+    });
 });
